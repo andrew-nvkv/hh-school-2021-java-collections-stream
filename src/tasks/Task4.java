ccf24daae5bb9fff8b4bf76d0b09661ae29540e0
Andrew Novikov <andrew.nvkv@gmail.com>
2021-12-20 07:27:05 +0300

    Task 4

--- src/tasks/Task4.java
+++ src/tasks/Task4.java
@@ -7,6 +7,7 @@ import common.Task;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Задача 4
@@ -18,8 +19,11 @@ FYI - DTO = Data Transfer Object - распространенный паттер
 public class Task4 implements Task {
 
   // !!! Редактируйте этот метод !!!
-  private List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
+  private List<ApiPersonDto> convert(List<Person> persons)
+  {
+    return persons.stream()
+            .map(Task4::convert)
+            .collect(Collectors.toList());
   }
 
   private static ApiPersonDto convert(Person person) {
