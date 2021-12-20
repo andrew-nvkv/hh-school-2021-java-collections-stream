d791de9de34f60c78562a3eba03ad95e3d27fd08
Andrew Novikov <andrew.nvkv@gmail.com>
2021-12-20 07:28:04 +0300

    Task 7

--- src/tasks/Task7.java
+++ src/tasks/Task7.java
@@ -7,14 +7,19 @@ import common.Vacancy;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
  */
 public class Task7 implements Task {
 
-  private Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
+  private Set<String> vacancyNames(Collection<Company> companies)
+  {
+    return companies.stream()
+            .flatMap(company -> company.getVacancies().stream())
+            .map(Vacancy::getTitle)
+            .collect(Collectors.toSet());
   }
 
   @Override
